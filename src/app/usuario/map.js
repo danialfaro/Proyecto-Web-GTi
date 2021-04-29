f037bf2ba3c864156b842006e42132b193ac8b55
Zero33w <guillem46723@gmail.com>
2021-04-30 01:40:55 +0200

    Marcas de mapa
    He añadido las marcas al mapa y ajustado el zoom y otros detalles

--- src/app/usuario/map.js
+++ src/app/usuario/map.js
@@ -1,8 +1,8 @@
 let map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -37.81439815430156, lng: 144.96480089398648},
-        zoom: 15,
+        center: {lat: -37.80030995551754, lng: 144.97832655465268},
+        zoom: 12,
         mapTypeId: 'hybrid',
         styles: [
             {
@@ -19,4 +19,23 @@ function initMap() {
         rotateControl: false,
         zoomControl:false,
     });
+	var marker1 = new google.maps.Marker({
+            position: {lat: -37.78535123595972, lng: 144.95436592384175},
+            label: "1",
+            animation: google.maps.Animation.DROP,
+            map: map
+    });
+	var marker2 = new google.maps.Marker({
+            position: {lat: -37.82092593042849, lng: 144.97874251347298},
+            label: "2",
+            animation: google.maps.Animation.DROP,
+            map: map
+    });
+	var marker3 = new google.maps.Marker({
+            position: {lat: -37.79155275567427, lng: 145.00957500199306},
+            label: "3",
+            animation: google.maps.Animation.DROP,
+            map: map
+    });
+
 }
