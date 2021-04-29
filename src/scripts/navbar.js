ba5e91caaf9fa5b86d14c39229d8c52539166988
danialfaro <infdalfamosni@gmail.com>
2021-04-29 18:13:51 +0200

    Navbar Loged

---
+++ src/scripts/navbar.js
@@ -0,0 +1,82 @@
+window.onload = () => {
+
+    var overlay = document.getElementById("overlay");
+    var activeOverlay = (active) => {
+        if (active) {
+            overlay.classList.remove('closed');
+        } else {
+            overlay.classList.add('closed');
+        }
+    }
+
+    /* Login Form Toggle Behaviour */
+
+    var isLoginOpen = false;
+    var loginToggleButton = document.getElementById("loginToggleButton");
+    var loginWrapper = document.getElementById("login-form-wrapper");
+
+    var openLoginMenu = () => {
+        isLoginOpen = true;
+        loginWrapper.classList.add("login-open");
+        loginWrapper.classList.remove("login-closed");
+        activeOverlay(true);
+    }
+
+    var closeLoginMenu = () => {
+        isLoginOpen = false;
+        loginWrapper.classList.remove("login-open");
+        loginWrapper.classList.add("login-closed");
+        activeOverlay(false);
+    }
+
+    loginToggleButton.addEventListener('click', () => {
+        isLoginOpen = !isLoginOpen;
+        isLoginOpen ? openLoginMenu() : closeLoginMenu();
+    });
+
+    /* End Login Form Toggle Behaviour */
+
+    /* Menu Toggle Behaviour */
+
+    var isMenuOpen = false;
+    var menuOpenButton = document.getElementById("menuOpenButton");
+    var menuCloseButton = document.getElementById("menuCloseButton");
+    var menu = document.getElementById("menu-emergente");
+
+    var openMenu = () => {
+        isMenuOpen = true;
+        menu.classList.add("menu-open");
+        menu.classList.remove("menu-closed");
+        closeLoginMenu();
+        activeOverlay(true);
+    }
+
+    var closeMenu = () => {
+        isMenuOpen = false;
+        menu.classList.remove("menu-open");
+        menu.classList.add("menu-closed");
+        activeOverlay(false);
+    }
+
+    menuOpenButton.addEventListener('click', openMenu);
+    menuCloseButton.addEventListener('click', closeMenu);
+
+    var menuLinks = menu.querySelectorAll('#menu-emergente ul li a');
+    menuLinks.forEach((link) => {
+        link.addEventListener('click', () => {
+            closeLoginMenu();
+            closeMenu();
+        });
+    })
+
+    /* End Menu Toggle Behaviour */
+
+    closeLoginMenu();
+    closeMenu();
+
+    overlay.addEventListener('click', () => {
+        if (isLoginOpen) closeLoginMenu();
+        if (isMenuOpen) closeMenu();
+    });
+
+}
