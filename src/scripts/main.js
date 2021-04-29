da6c79168ddbf6550e4b466b35644ed41f0ebc55
danialfaro <infdalfamosni@gmail.com>
2021-04-30 00:49:29 +0200

    Admin y User permisos

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,24 @@
-window.onload = () => {
+/* FAQ Toggle Behaviour */
 
-    /* FAQ Toggle Behaviour */
+var faqSection = document.getElementById("sectionFaq");
+var faqItems = faqSection.querySelectorAll(".faq_item");
+faqItems.forEach((faqItem) => {
 
-    var faqSection = document.getElementById("sectionFaq");
-    var faqItems = faqSection.querySelectorAll(".faq_item");
-    faqItems.forEach((faqItem) => {
+    var trigger = faqItem.getElementsByClassName("faq_trigger")[0];
 
-        var trigger = faqItem.getElementsByClassName("faq_trigger")[0];
+    trigger?.addEventListener('click', () => {
+        if(!faqItem.classList.contains("faq_open")) {
+            faqItems.forEach((fi) => {
+                fi.classList.remove("faq_open");
+            });
+            faqItem.classList.add("faq_open");
+        } else {
+            faqItem.classList.remove("faq_open");
+        }
+    });
 
-        trigger?.addEventListener('click', () => {
-            if(!faqItem.classList.contains("faq_open")) {
-                faqItems.forEach((fi) => {
-                    fi.classList.remove("faq_open");
-                });
-                faqItem.classList.add("faq_open");
-            } else {
-                faqItem.classList.remove("faq_open");
-            }
-        });
+})
 
-    })
-
-    /* End FAQ Toggle Behaviour */
+/* End FAQ Toggle Behaviour */
 
 
-}
