c25f90920cc9c97f8afcdec1ab08eac1e58319e8
Zero33w <guillem46723@gmail.com>
2021-05-13 21:24:56 +0200

    Popup condicionado
    Ahora para que aparezca el popup es necesario que todos los campos esten rellenados correctamente

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -3,12 +3,32 @@ var btnAbrirPopup = document.getElementById('popupContact'),
     popup = document.getElementById('popupID'),
     btnCerrarPopup = document.getElementById('btn-cerrar-popup');
 
-btnAbrirPopup.addEventListener('click',function (){
-    overlay.classList.add('active');
-    popup.classList.add('active');
+var mail = document.getElementById("mailOK");
+var nombre = document.getElementById("nombreOK");
+var asunto = document.getElementById("asuntoOK");
+var text = document.getElementById("mensaje");
+
+
+
+mail.addEventListener("input", function (event) {
+	
+	console.log("Mail: "+mail.validity.typeMismatch);
+	console.log("Nombre: "+nombre.validity.valueMissing);
+	console.log("Asunto: "+asunto.validity.valueMissing);
+	console.log("Text: "+text.validity.valueMissing);
+	if (mail.validity.typeMismatch || nombre.validity.valueMissing ||asunto.validity.valueMissing  ||text.validity.valueMissing ) {} 
+	else {
+  
+		btnAbrirPopup.addEventListener('click',function (){
+			overlay.classList.add('active');
+			popup.classList.add('active');
+		});
+
+		btnCerrarPopup.addEventListener('click',function (){
+		  overlay.classList.remove('active');
+		  popup.classList.remove('active');
+		});
+	}
 });
 
-btnCerrarPopup.addEventListener('click',function (){
-    overlay.classList.remove('active');
-    popup.classList.remove('active');
-});
+
