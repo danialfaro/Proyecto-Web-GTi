8b6e06bd821b033b2656c82df9c05357f2e43c02
Zero33w <guillem46723@gmail.com>
2021-05-26 01:41:25 +0200

    correccion del post sonda

--- src/api/v1.0/modelos/sondas/post-sondas.php
+++ src/api/v1.0/modelos/sondas/post-sondas.php
@@ -1,28 +1,28 @@
 <?php
+if(!isset($conn)) { die(); }
+
+if(empty($_POST["mac"])) {
+    http_response_code(400);
+    die();
+}
 // Procesar los datos de la peticion
-$sondaMAC = $_POST['mac'];
+$mac= $_POST['mac'];
 
 // Query para buscar la sonda
+
 //$sql = "SELECT * FROM sondas";
-$sql = "SELECT * FROM sondas WHERE mac = '$sondaMAC' ";
+$sql = "INSERT INTO `sondas`(`mac`) 
+        VALUES ('$mac')";
 
 // Ejecutar la consulta a la BD
 $result = mysqli_query($conn, $sql);
 
-// Procesar el resultado de la consulta
-if(mysqli_num_rows($result) > 0) {
+if($result) {
+    $id = mysqli_insert_id($conn);
+    $respuesta["id"] = $id;
 
-    session_start();
-
-    while ($fila = mysqli_fetch_assoc($result)) {
-
-        // Iniciar variables de respuesta
-        $respuesta = $fila;
-    }
-
-    http_response_code(200);
 } else {
-    // No hay resultados en la consulta
+    // No se ha podido realizar la accion
     http_response_code(401);
     die();
 }
